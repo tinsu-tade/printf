58ded1a750597aff94251991c82dc39c236d8800
tinsae1523 <74678754+tinsu-tade@users.noreply.github.com>
2021-11-23 09:01:47 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -1,1 +1,62 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include<stdarg.h>
+#include "main.h"
 
+int _printf(const char *format, ...)
+{
+int characterCount ;
+int printInteger;
+char integerString[10];
+char *pPrintString;
+va_list vaList;
+unsigned int charToPrint;
+va_start (vaList, pszFormat);
+
+while(*format)
+{
+if(*format == '%')
+{
+format++;
+switch(*format)
+{
+case 'c':
+charToPrint= va_arg(vaList,int );
+putchar(charToPrint);
+format++;
+characterCount++;
+break;
+
+case 's':
+pPrintString = va_arg(vaList,char *);
+fputs(pPrintString,stdout);
+format++;
+characterCount += strlen(pPrintString);
+break;
+    
+case '%':
+putchar('%');
+format++;
+characterCount++;
+
+
+case '\0':                           
+break;
+
+default:
+//putchar('%');
+putchar(*format);
+format++;
+characterCount += 2;
+}
+}
+else
+{
+putchar(*format);
+format++;
+characterCount++;
+}
+} 
+va_end(vaList);
+return (characterCount); 
+}
